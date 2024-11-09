de2195d8c05d8bc70a39a427b31d3b47792378a0
Junchannn <146307390+Junchannn@users.noreply.github.com>
2024-11-09 21:39:58 +0700

    Update models.py

--- nmcnpmapp/models.py
+++ nmcnpmapp/models.py
@@ -1,33 +1,47 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
-
-class CustomUser(AbstractUser):
-    is_approved = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    group = models.CharField(max_length=30)
-    name = models.CharField(max_length=100)
-    dongphi = models.BooleanField(default=False)
-    donggop = models.PositiveSmallIntegerField(default=0)
-    sukien = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return self.username
-    
 from django.conf import settings
 
-class FamilyMember(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-    group = models.CharField(max_length=50, default='')
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-
+class CustomUser(models.Model):
+    room_id = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    is_approved = models.BooleanField(default=False)
+    registry_email = models.EmailField(default="lol@gamil.com")
+    phone_number = models.CharField(max_length=10, default="0123456789")
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
-    
+        return self.username
+#Store payment details
+class Payment(models.Model):
+    payment = models.CharField(max_length=255)
+    amount = models.PositiveIntegerField(default=0)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.payment} - {self.amount}"
+
+#Table to store payment status of each room
+class PaymentStatus(models.Model):
+    room_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, to_field='room_id', related_name="payment_statuses")
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name="statuses")
+    status = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.room_id.room_id} - {self.payment.payment} - {self.status}"
+#Store family members details
+class FamilyMember(models.Model):
+    id = models.AutoField(primary_key=True)
+    room_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, to_field='room_id', related_name="family_members")
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(default=13)
+    email = models.EmailField(default="lol@gamil.com")
+    phone_number = models.CharField(max_length=10, default="0123456789")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+#Store notification details
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
